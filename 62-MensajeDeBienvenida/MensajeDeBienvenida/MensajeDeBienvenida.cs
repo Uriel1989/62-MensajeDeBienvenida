11509e3efaea8fcfed0b0c607005f5783c1b2713
Uriel Guillermo Daniel <45216219+Uriel1989@users.noreply.github.com>
2022-06-15 21:23:49 -0300

    Add files via upload

---
+++ 62-MensajeDeBienvenida/MensajeDeBienvenida/MensajeDeBienvenida.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MensajeDeBienvenida
+{
+    class MensajeDeBienvenida
+    {
+        public string NombreDePersona ( string Nombre)
+        {
+            return Nombre;
+        }
+
+        public string NombreDeCiudad (string Ciudad)
+        {
+            return Ciudad;
+        }
+    }
+}
---
+++ 62-MensajeDeBienvenida/MensajeDeBienvenida/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MensajeDeBienvenida
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MensajeDeBienvenida Extrangero = new MensajeDeBienvenida();
+
+            Console.CursorLeft = 30;
+            Console.WriteLine("Ingrese el nombre de una personas y de una ciudad:");
+            string Nombre = Convert.ToString(Console.ReadLine());
+            string Ciudad = Convert.ToString(Console.ReadLine());
+
+            Console.Clear();
+
+            Console.WriteLine("Hola " + Extrangero.NombreDeCiudad(Nombre)+"," + " bienvenido a " + Extrangero.NombreDeCiudad(Ciudad));
+
+            Console.ReadKey();
+        }
+    }
+}
